ade184c69482ca0bf355bd8a66eec75c949f7785
ygeum2 <ygeum1@gmail.com>
2022-07-14 07:29:58 +0900

    클래스와 객체

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -8,22 +8,45 @@ public class Student {
 	int grade;
 	String address;
 	
+	// 생성자 overloading
+	// (동일한 이름의 method 가 매개변수가 다른경우)
+	public Student() {} 
+	// default 생성자(= 기본생성자)
+	// 매개변수가 없음
+	// 생성자가 하나도 없을때 생성됨.
+	// 하지만 생성자가 하나라도 있다면 기본 생성자가 support 되지 않지만,
+	// 필요하다면 직접 코딩(public Student() {} )하여 사용할 수 있음.
+	// 이 개체가 생성될때 기본적으로 하고 싶은 일들을 구현.
+
+	// id 와 이름을 매개변수로 입력받는 생성자.
+	public Student(int id, String name) {
+		studentID = id;
+		studentName = name;
+	}
+	
+	
+	
 	public void showStudentInfor( ) {
 		System.out.println(studentName + ", " + address);
 	}
 	
-	// class 를 수행(run 구동부분)하기 위한 main method 로서 형식이 동일함.
-	// 하지만 main 부분은 Student 의 method 라고 부르지 않음.
-	// 또한 이 main method 가 항상 같은 java 파일에 있는 것이 아니라,
-	// 다른 class 에서 호출하는 것도 가능함. 
-	public static void main(String[] args) {
-		
-		Student studentLee = new Student();
-		studentLee.studentName = "이순신";
-		studentLee.address = "서울시 서초구 서초동";
-		
-		studentLee.showStudentInfor();
+	public String getStudentName() {
+		// 해당 클래스를 사용하는 입장에서 네이밍(get)하는 것이 좋음.
+		return studentName;
 	}
+	
+	// 이름 바꾸는 class
+	public void setStudentName(String name) {
+		// 반환값이 없는 경우 void 를 붙여줌.
+		studentName = name;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
 
--- Chapter5/src/classpart/StudentTest.java
+++ Chapter5/src/classpart/StudentTest.java
@@ -3,12 +3,40 @@ package classpart;
 public class StudentTest {
 
 	public static void main(String[] args) {
+	// 이 class 에 정의되어있지는 않지만,
+	// 다른 class 에 정의된 Student class 를 호출할 수 있음.
+		
+	// class 를 수행(run 구동부분)하기 위한 main method 로서 형식이 동일함.
+	// 하지만 main 부분은 Student 의 method 라고 부르지 않음.
+	// 또한 이 main method 가 항상 같은 java 파일에 있는 것이 아니라,
+	// 다른 class(StudentTest.java) 에서 호출하는 것도 가능함. 
 		
 		Student studentLee = new Student();
+		// Student 가 참조형 데이터 타입.
+		// studentLee 가 참조변수, Student() 가 생성자.
+		// studentID, studentName, grade, address 가 멤버변수.
+		studentLee.studentID = 100;
 		studentLee.studentName = "이순신";
 		studentLee.address = "서울시 서초구 서초동";
+
+		Student studentKim = new Student();
+		// default 생성자가 있다면, new Student(101, "김유신"); 으로 작성하거나,
+		// 생성자 overloading 을 함.
+		studentKim.studentID = 101;
+		studentKim.studentName = "김유신";
+		studentKim.address = "미국 산호세";
 		
 		studentLee.showStudentInfor();
+		studentKim.showStudentInfor();
+		
+		System.out.println(studentLee);  // classpart.Student@77556fd
+		System.out.println(studentKim);  // classpart.Student@368239c8
+		// classpart.Student 는 Studnet class 의 패키지 이름까지 포함한 풀네임.
+		// 77556fd 와 368239c8 는 16진수로 4byte * 8자리 = 32bit 의 address(참조값).
+		// studentLee, studentKim 가 stack 에서 
+		// Heap 에 저장된 각각의 멤버변수의 주소를 가르키고 있음.
 	}
-
+	
 }
+
+
